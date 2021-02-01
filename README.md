00838e33ed4767e156ed8d1b391e733ed2839111
czl0325 <>
2021-02-01 13:22:23 +0800

    vue学习笔记。开发一个组件

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# zhihu-vue3
-入手vue3.0，开发知乎项目
+# zhihu
+
+#### 编译
+`npm install`
+#### 运行
+`npm run serve`
+#### 学习笔记
+1. [vue3.0学习笔记1---项目创建](https://blog.csdn.net/czl0325/article/details/113504678)
+2. [vue3.0学习笔记2---尝试开发一个组件](https://blog.csdn.net/czl0325/article/details/113507369)
--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,29 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div class="container">
+    <column-list :list="list"></column-list>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import { defineComponent } from 'vue'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import ColumnList, { ColumnProps } from './components/ColumnList.vue';
+const testData:ColumnProps[] = [
+  {id:1, title: '专栏1', description: '这是专栏1', avatar: 'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fwx4.sinaimg.cn%2Flarge%2F007s9uFmgy1gn7wnixsccj30k00aijrn.jpg&refer=http%3A%2F%2Fwx4.sinaimg.cn&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1614747631&t=5c5e2ffc44d0dfa3af13b7f4e0c6e12e'},
+  {id:2, title: '专栏2', description: '这是专栏2', avatar: 'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fn.sinaimg.cn%2Fsinakd10111%2F145%2Fw608h337%2F20210201%2Fde7f-kiksqxh5466822.jpg&refer=http%3A%2F%2Fn.sinaimg.cn&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1614747653&t=1c3578cf07c53431faed8847eeef645e'}
+]
 
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
+    ColumnList
+  },
+  setup() {
+    return {
+      list: testData
+    }
   }
-});
+})
 </script>
 
 <style>
@@ -22,6 +33,5 @@ export default defineComponent({
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
