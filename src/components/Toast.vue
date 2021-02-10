00fbc73f11edbe2868718ae542b7b663df61adb6
陈昭良 <295183917@qq.com>
2021-02-10 12:10:48 +0800

    创建toast

---
+++ src/components/Toast.vue
@@ -0,0 +1,38 @@
+<template>
+  <teleport >
+
+  </teleport>
+</template>
+
+<script lang="ts">
+import {defineComponent} from 'vue'
+const MessageType: string = 'success' | 'error' | 'default';
+
+export default defineComponent({
+  name: "Toast",
+  props: {
+    message: String,
+    type: {
+      type: MessageType,
+      default: 'default'
+    }
+  },
+  setup() {
+
+  }
+})
+</script>
+
+<style scoped>
+.loading-content {
+  background-color: rgba(0, 0, 0, 0.3);
+  z-index: 100;
+  position: fixed;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  line-height: 100vh;
+  text-align: center;
+}
+</style>
